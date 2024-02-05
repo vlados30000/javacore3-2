303d2ad38ce247be49b50b303a8dfea4273e1584
Vlad <vladekshibarov@yandex.ru>
2024-02-05 19:51:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+
+        //  несовершеннолетние
+        long underages = persons.stream().filter(person -> person.getAge() < 18).count();
+
+        // призывники
+        List<String> recruit = persons.stream().filter(person -> (person.getAge() >= 18 && person.getAge() <= 27))
+                .filter(person -> person.getSex() == Sex.MAN).map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        // высшие образование
+        Collection<Person> ableBodied = persons.stream().
+                filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getSex() == Sex.MAN ? person.getAge() >= 18 && person.getAge() <= 65
+                        : person.getAge() >= 18 && person.getAge() <= 60)
+                .sorted(Comparator.comparing(Person::getFamily)).collect(Collectors.toList());
+        
+    }
+}
